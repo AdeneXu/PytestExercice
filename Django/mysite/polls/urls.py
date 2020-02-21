4519e94ba39b58bbef9efcadd9ac92969987c04b
AdeneXu <869656101@qq.com>
2020-02-21 17:50:12 +0800

    Code optimization

--- Django/mysite/polls/urls.py
+++ Django/mysite/polls/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     # ex: /polls/5/
     path('<int:pk>/',views.DetailView.as_view(),name='detail'),
     #ex: /polls/5/results/
-    path('<int:pk>/results/',views.ResultView.as_view(),name='results'),
-    path('<int:pk>/vote/',views.vote,name='vote'),
+    path('<int:pk>/results/',views.ResultsView.as_view(),name='results'),
+    path('<int:question_id>/vote/',views.vote,name='vote'),
 ]
