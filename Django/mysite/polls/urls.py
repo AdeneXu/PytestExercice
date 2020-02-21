78273b97b9d06ac8d76f4eecbda48dd8dec356da
AdeneXu <869656101@qq.com>
2020-02-21 17:05:33 +0800

    Django training

--- Django/mysite/polls/urls.py
+++ Django/mysite/polls/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('',views.index,name='index')
+    #ex: /polls/
+    path('',views.IndexView.as_view(),name='index'),
+    # ex: /polls/5/
+    path('<int:pk>/',views.DetailView.as_view(),name='detail'),
+    #ex: /polls/5/results/
+    path('<int:pk>/results/',views.ResultView.as_view(),name='results'),
+    path('<int:pk>/vote/',views.vote,name='vote'),
 ]
